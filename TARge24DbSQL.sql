e5c8489fe42d7a61bb1afb7f1e9688fbbebd2cd1
Ingvar Derešivski <inde999@gmail.com>
2025-03-12 15:06:18 +0200

    12.03.2024

--- TARge24DbSQL.sql
+++ TARge24DbSQL.sql
@@ -136,4 +136,231 @@ select top 3 * from person
 -- kolm esimest, aga tabeli järjestus on Age ja siis Name
 select top 3 Age, Name from Person
 
--- 3tund rida 135
+-- 3tund 12.03.2025
+
+--näita esimesed 50% tabelis
+select top 50 percent * from Person
+
+--järjestab vanuse järgi isikud
+-- see päring ei järjesta numbreid õigesti kuna Age on nvarchar
+select * from Person order by Age desc
+
+--castime Age int andmetüübiks ja siis järjestab õigesti
+select * from Person order by CAST(Age as int)
+
+-- kõikide isikute koondvanus
+select sum(cast(Age as int)) as [Total Age] from Person
+
+--k]ige noorem isik
+select min(cast(Age as int)) from Person
+--k]ige vanem isik
+select max(cast(Age as int)) from Person
+
+-- n'eme konkreetsetes linnades olevate isikute koondvanust
+-- enne oli Age nvarchar, aga enne päringut muudame selle int-ks
+select City, sum(Age) as TotalAge from Person group by City
+
+-- nüüd muudame muutuja andmetüüpi koodiga
+alter table Person
+alter column Name nvarchar(25)
+
+--- kuvab esimeses reas välja toodud järjestuses ja kuvab Age-i TotalAge-ks
+--- järjestab City-s olevate nimede järgi ja siis GenderId järgi
+select City, GenderId, sum(Age) as TotalAge from Person
+group by City, GenderId order by City
+
+--- näitab, et mitu rida on selles tabelis
+select count(*) from Person
+select * from Person
+
+--- näitab tulemust, et mitu inimest on GenderId väärtusega 2 konkreetses linnas
+-- veel arvutab vanuse kokku
+select City, GenderId, sum(Age) as TotalAge, count(Id)as [Total Person(s)]
+from Person
+where GenderId = '2'
+group by GenderId, City
+
+insert into Person values
+(10, 'Black Panther', 'b@b.com', 2, 34, 'New York')
+
+--- näitab ära inimeste koondvanuse, kelle vanus on vähemalt 29 a
+-- kui palju neid igas linnas elab
+select GenderId, City, sum(Age) as TotalAge, count(Id)
+as [Total Person(s)]
+from Person
+group by GenderId, City having sum(Age) > 29
+
+
+--loome tabelid Employees ja Department
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(50),
+Salary nvarchar(50),
+DepartmentId int
+)
+select * from Employees
+
+
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (1, 'Tom', 'Male', 4000, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (2, 'Pam', 'Female', 3000, 3)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (3, 'John', 'Male', 3500, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (4, 'Sam', 'Male', 4500, 2)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (5, 'Todd', 'Male', 2800, 2)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (6, 'Ben', 'Male', 7000, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (7, 'Sara', 'Female', 4800, 3)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (8, 'Valarie', 'Female', 5500, 1)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (9, 'James', 'Male', 6500, NULL)
+insert into Employees (Id, Name, Gender, Salary, DepartmentId)
+values (10, 'Russell', 'Male', 8800, NULL)
+
+insert into Department(Id, DepartmentName, Location, DepartmentHead)
+values 
+(1, 'IT', 'London', 'Rick'),
+(2, 'Payroll', 'Delhi', 'Ron'),
+(3, 'HR', 'New York', 'Christie'),
+(4, 'Other Department', 'Sydney', 'Cindrella')
+
+select * from Department
+select * from Employees
+
+--left join
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+
+-- arvutab k]ikide palgad kokku
+select sum(cast(Salary as int)) from Employees
+-- k]ige v'iksema palgasaaja palk
+select min(cast(Salary as int)) from Employees
+--ühe kuu palgafond linnade lõikes
+select Location, sum(cast(Salary as int)) as TotalSalary
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+group by Location
+
+alter table Employees
+add City nvarchar(30)
+
+--sooline erip'ra palkade osas
+select City, Gender, sum(cast(Salary as int)) as TotalSalary 
+from Employees
+group by City, Gender
+-- sama nagu eelmine, aga linnad on t'hestikulises j'rjekorras
+select City, Gender, sum(cast(Salary as int)) as TotalSalary 
+from Employees
+group by City, Gender
+order by City
+
+-- loeb ära, mitu rida andmeid on tabelis,
+-- * asemele võib panna ka muid veergude nimetusi
+select count(DepartmentId) from Employees
+
+--mitu töötajat on soo ja linna kaupa selles tabelis
+select Gender, City,
+count (Id) as [Total Employee(s)]
+from Employees
+group by Gender, City
+
+-- näitab kõik mehed linnade kaupa
+select Gender, City,
+count (Id) as [Total Employee(s)]
+from Employees
+where Gender = 'Male'
+group by Gender, City
+
+-- näitab kõik naised linnade kaupa
+select Gender, City,
+count (Id) as [Total Employee(s)]
+from Employees
+group by Gender, City
+having Gender = 'Female'
+
+-- kelle palk on vähemalt üle 4000
+select * from Employees where sum(cast(Salary as int)) > 4000
+-- nüüd õige päring
+select * from Employees where Salary > 4000
+
+-- k]igil,kellel on palk üle 4000 ja arvutab need kokku ning näitab soo kaupa
+select Gender, City, sum(cast(Salary as int)) as TotalSalary, count (Id) as [Total Employee(s)]
+from Employees group by Gender, City
+having sum(cast(Salary as int)) > 4000
+
+--loome tabeli, milles hakatakse automaatselt nummerdama Id-d
+create table Test1
+(
+Id int identity(1,1),
+Value nvarchar(20)
+)
+insert into Test1 values('X')
+select * from Test1
+
+--kustutage ära City veerg Employees tabelist
+alter table Employees
+drop column City
+
+--- inner join
+-- kuvab neid, kellel on DepartmentName all olemas väärtus
+select Name, Gender, Salary, DepartmentName
+from Employees
+inner join Department
+on Employees.DepartmentId = Department.Id
+
+-- left join
+-- kuidas saada k]ik andmed Employees tabelist kätte
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department -- võib kasutada ka LEFT OUTER JOIN-i
+on Employees.DepartmentId = Department.Id
+
+-- right join
+-- kuidas saada Deparmtentname alla uus nimetus e antud juhul Other Department
+select Name, Gender, Salary, DepartmentName
+from Employees
+right join Department
+on Employees.DepartmentId = Department.Id
+
+-- full outer join
+--- kuidas saada kõikide tabelite väärtused ühte päringusse
+select Name, Gender, Salary, DepartmentName
+from Employees
+full outer join Department
+on Employees.DepartmentId = Department.Id
+
+--- cross join võtab kaks allpool olevat tabelit kokku ja korrutab need omavahel läbi
+select Name, Gender, Salary, DepartmentName
+from Employees
+cross join Department
+
+--päringu sisu e loogika
+select ColumnList
+from LeftTable
+joinType RightTable
+on JoinCondition
+
+--- kuidas kuvada ainult need isikud, kellel on DepartmentName NULL
+select Name, Gender, Salary, DepartmentName
+from Employees
+left join Department
+on Employees.DepartmentId = Department.Id
+where Employees.DepartmentId is null
