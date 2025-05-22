11d5a700f80815499fee7f502c9cc3d9302c4437
MSI\ingva <inde999@gmail.com>
2025-05-22 13:22:59 +0300

    22.05.2025

--- TARge24DbSQL.sql
+++ TARge24DbSQL.sql
@@ -2958,3 +2958,185 @@ truncate table TableB
 --- mille tagasi viimine on kõige vähem ressurssi nõudev.
 --- 3. Kui mõlemal sessioonil on sama prioriteet ja sama 
 --- ressursi kulutamine, siis ohver valitakse juhuslikuse alusel
+
+truncate table TableA
+truncate table TableB
+
+insert into TableA values('Mark')
+insert into TableA values('Ben')
+insert into TableA values('Todd')
+insert into TableA values('Pam')
+insert into TableA values('Sara')
+
+insert into TableB values('Mary')
+
+-- tran 1 e server nr 1
+-- samm nr 1
+begin tran
+update TableA set Name = Name +
+'Transaction 1' where Id in (1, 2, 3, 4, 5)
+
+-- samm nr 3
+update TableB set Name = Name +
+'Transaction 1' where Id = 1
+
+-- samm 5
+commit tran
+
+-- server nr 2
+-- samm nr 2
+set deadlock_priority high
+go
+begin tran
+update TableB set Name =
+Name + 'Transaction 1' where Id = 1
+
+-- samm nr 4
+update TableA set Name = 
+Name + 'Transaction 1' where Id
+in (1, 2, 3, 4, 5)
+
+-- samm nr 6
+commit tran
+
+-- deadlocki logimine
+
+dbcc Traceon(1222, -1)
+
+dbcc TraceStatus(1222, -1)
+
+dbcc TraceOff(1222, -1)
+
+
+truncate table TableA
+truncate table TableB
+
+-- vaja teha stored procedure: spTransaction1
+-- sinna sisse vaja luua transaction, mis uuendab andmeid
+-- tableA-s ja lõpus teeb TableB-s uuenduse
+-- nende kahe uuenduse vahel on aeg 10 sek
+-- TableA teksti uuendus on Mark Transaction 1
+-- ja TableB uuendus on: Mary Transaction 1
+
+insert into TableA values('Mark')
+insert into TableB values('Mary')
+
+create proc spTransaction1
+as begin
+	begin tran
+	update TableA set Name = 'Mark Transaction 1' where Id = 1
+	waitfor delay '00:00:10'
+	update TableB set Name = 'Mary Transaction 1' where Id = 1
+	commit tran
+end
+
+alter proc spTransaction2
+as begin
+	begin tran
+	update TableB set Name = 'Mark Transaction 2' where Id = 1
+	waitfor delay '00:00:10'
+	update TableA set Name = 'Mary Transaction 2' where Id = 1
+	commit tran
+end
+
+exec spTransaction1
+exec spTransaction2
+
+select * from TableA
+select * from TableB
+
+-- errorlogi kuvamine
+execute sp_readerrorlog
+
+-- kuidas koodi abil vaadata errorit
+select OBJECT_NAME([object_Id]) -- ei ole hetkel objekti id-d
+from sys.partitions
+where hobt_id = 12345467   -- numbrite jada
+
+-- muuta spTransaction1
+-- peab kasutama vea käsitlemist try ja catchiga
+-- update mitte muuta
+-- kui transaction lõppeb, siis tuleb: select 'Transaction Successful'
+-- catchi sees omakorda if ja tingimuseks on ERROR_NUMBER() = 1205
+alter proc spTransaction1
+as begin
+	begin tran
+	begin try
+		update TableB set Name = 'Mark Transaction 1' where Id = 1
+		waitfor delay '00:00:10'
+		update TableA set Name = 'Mary Transaction 1' where Id = 1
+		commit tran
+		select 'Transaction Successful'
+	end try
+	begin catch
+		--vaatab, kas see error on deadlocki oma
+		if(ERROR_NUMBER() = 1205)
+		begin
+			select 'Deadlock. Transaction failed. Please retry'
+		end
+
+		rollback
+	end catch
+end
+-- teisega samamoodi
+alter proc spTransaction2
+as begin
+	begin tran
+	begin try
+		update TableB set Name = 'Mark Transaction 2' where Id = 1
+		waitfor delay '00:00:10'
+		update TableA set Name = 'Mary Transaction 2' where Id = 1
+		commit tran
+		select 'Transaction Successful'
+	end try
+	begin catch
+		--vaatab, kas see error on deadlocki oma
+		if(ERROR_NUMBER() = 1205)
+		begin
+			select 'Deadlock. Transaction failed. Please retry'
+		end
+
+		rollback
+	end catch
+end
+
+-- käivitad esimeses serveris
+exec spTransaction1
+
+-- käivita teises serveris
+exec spTransaction2
+
+SELECT
+    [s_tst].[session_id],
+    [s_es].[login_name] AS [Login Name],
+    DB_NAME (s_tdt.database_id) AS [Database],
+    [s_tdt].[database_transaction_begin_time] AS [Begin Time],
+    [s_tdt].[database_transaction_log_bytes_used] AS [Log Bytes],
+    [s_tdt].[database_transaction_log_bytes_reserved] AS [Log Rsvd],
+    [s_est].text AS [Last T-SQL Text],
+    [s_eqp].[query_plan] AS [Last Plan]
+FROM
+    sys.dm_tran_database_transactions [s_tdt]
+JOIN
+    sys.dm_tran_session_transactions [s_tst]
+ON
+    [s_tst].[transaction_id] = [s_tdt].[transaction_id]
+JOIN
+    sys.[dm_exec_sessions] [s_es]
+ON
+    [s_es].[session_id] = [s_tst].[session_id]
+JOIN
+    sys.dm_exec_connections [s_ec]
+ON
+    [s_ec].[session_id] = [s_tst].[session_id]
+LEFT OUTER JOIN
+    sys.dm_exec_requests [s_er]
+ON
+    [s_er].[session_id] = [s_tst].[session_id]
+CROSS APPLY
+    sys.dm_exec_sql_text ([s_ec].[most_recent_sql_handle]) AS [s_est]
+OUTER APPLY
+    sys.dm_exec_query_plan ([s_er].[plan_handle]) AS [s_eqp]
+ORDER BY
+    [Begin Time] ASC;
+GO
